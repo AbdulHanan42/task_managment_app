af462b88898c018a749c23656061cd07ef3f21e3
AbdulHanan <abdulhananjaved4412@gmail.com>
2026-09-24 17:55:06 +0500

    add the authentication for the user register

---
+++ src/user/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, String, Integer, Boolean, DateTime
+from src.utils.db import Base
+
+class  UserModel(Base):
+    __tablename__ = "users_table"
+
+    id = Column (Integer, primary_key=True)
+    name = Column(String)
+    username = Column(String, nullable=False)
+    hash_password = Column(String, nullable=False)
+    email = Column(String)
+
+
+
